d4735561ab2aba1761a5c52b8137edc4f40e3207
Jeff Pancerz <pancerzj@gmail.com>
2022-03-16 09:20:21 -0500

    syncing files

--- myscript.js
+++ myscript.js
@@ -1,94 +1,60 @@
-//notes to self:
-// left off on the final step, have to figure out a way to keep score at the end of each round of play
-// the 'score' has to persist and update through all five games and then declare a winner via console.log at the end of the matches.
+//notes to self: game working correctly 'finally', now
+//i need to create a scoreboard that updates at the end of each round
+//ill do this by creating divs inside the html
+//ill then make the html elements into 'dom objects' by creating them inside my js file using let/const
+// i need to add some form of logic to the end of the game function that updates at the end of each round and reflects in the score
+// the score object will have to be updated on screen.
+//announce a winner (alert?) after a player has reached a total of 5 points.
 
+///AFTER GAME IS DONE TRY MAKING SELECTION FOR PLAYER WITH SOLEY CLICKING ON BUTTON
+//NO ""HARD CODING"" values into each button press.
 
-// a valid set of inputs for the computer and player to choose from
-const selectMove = ['rock', 'paper', 'scissors'];
+//global variables 
+computerSelection = ['rock','paper','scissors']
 let playerScore = 0;
 let computerScore = 0;
 
-// compares the results of the entire series of 5 games and declares a overall winner.
-function compareResults(){
-    playerScore;
-    computerScore;
-    if(playerScore > computerScore){
-        console.log('congrats, you\'v won!')
-    }
-    else if (playerScore === computerScore){
-        console.log('this match is a draw!')
-    }
-    else {
-        console.log ('better luck next time, you lost.')
-    }
-}
+//creates functional DOM objects to select.
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 
-//logic for the computer to select a choice at random from RPS
-function computerPlayer() {
-    computerChoice = selectMove[Math.floor(Math.random() * selectMove.length)];
-    console.log (`The computer chooses ${computerChoice}!`);
-    return computerChoice;
-}
+//logic for buttons when clicked.
+rock.addEventListener("click", function(e){
+    game('rock');
+})
+paper.addEventListener("click", function(e){
+    game('paper')
+})
+scissors.addEventListener("click", function(e){
+    game('scissors')
+})
 
-//logic for the player choice.
-function player() {
-    playerChoice = prompt('rock, paper, scissors?').toLowerCase();
-    console.log(`You choose ${playerChoice}!`);
-    return playerChoice;
-}
-
-//plays a single round of RPS and tracks score after each round of play
-function playRound(playerSelection, computerSelection){
-    player();
-    computerPlayer();
-    computerScore;
-    playerScore;
-
-    //in event of a tie 
-    if (playerChoice === computerChoice) {
-        console.log('its a tie!')
-    }
-    //if player beats computer
-    else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-        console.log  ('you won! rock breaks scissors.') 
-        playerScore++
-        console.log(`human score - ${playerScore} vs computer score - ${computerScore}`)
-    }   
-    else if (playerChoice === 'paper' && computerChoice === 'rock') {
-        console.log('you won! paper covers rock.') 
-        playerScore++
-        console.log(`human score - ${playerScore} vs computer score - ${computerScore}`)
-    }
-    else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-        console.log('you won! scissors shreds paper.') 
-        playerScore++
-        console.log(`human score - ${playerScore} vs computer score - ${computerScore}`)
-    }
-    //if computer beats player
-    else if (playerChoice === 'paper' && computerChoice === 'scissors') {
-        console.log('computer wins, scissors shreds paper.') 
-        computerScore++
-        console.log(`human score - ${playerScore} vs computer score - ${computerScore}`)
-    }
-    else if (playerChoice === 'scissors' && computerChoice === 'rock') {
-        console.log('computer wins, rock breaks scissors.') 
-        computerScore++
-        console.log(`human score - ${playerScore} vs computer score - ${computerScore}`)
-    }
-    else if (playerChoice === 'rock' && computerChoice === 'paper') {
-        console.log('computer wins, paper covers rock.') 
-        computerScore++
-        console.log(`human score - ${playerScore} vs computer score - ${computerScore}`)
-    }
-    return;
-}
-
-//plays a series of 5 games of RPS.
-function game() {
-    for (let roundCount = 0; roundCount < 5; roundCount++) {
-        playRound()
+//play a single round of RPS vs the computer.
+function game(player){
+        const computer = computerSelection[Math.floor(Math.random() * computerSelection.length)]
+        if (player === computer) {
+            console.log('its a tie!')
         }
+        //if player beats computer
+        else if (player === 'rock' && computer === 'scissors') {
+            console.log  ('you won! rock breaks scissors.') 
+        }   
+        else if (player === 'paper' && computer === 'rock') {
+            console.log('you won! paper covers rock.') 
+        }
+        else if (player === 'scissors' && computer === 'paper') {
+            console.log('you won! scissors shreds paper.') 
+        }
+        //if computer beats player
+        else if (player === 'paper' && computer === 'scissors') {
+            console.log('computer wins, scissors shreds paper.') 
+        }
+        else if (player === 'scissors' && computer === 'rock') {
+            console.log('computer wins, rock breaks scissors.') 
+        }
+        else if (player === 'rock' && computer === 'paper') {
+            console.log('computer wins, paper covers rock.') 
+        }
+        return;
     }
-
-game()
-compareResults()
